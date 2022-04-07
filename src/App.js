950188afb4758cc0a73fc194365736d00115ed68
Rozina <rozina@Aamirs-MBP.broadband>
2022-04-07 18:59:36 +0100

    created a NavBar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import './App.css';
+import React from 'react';
+import { Container, Navbar, Nav } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <h1> Watch this space, launching soon. </h1> </div>
-  );
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Rozina</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">About</Nav.Link>
+            <Nav.Link href="#features">Game</Nav.Link>
+            <Nav.Link href="#pricing">Contact</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <About />
+    </>
+  )
+  function About() {
+    return (
+      <p>This is the story all about how my life got flipped turned...</p>
+    )
+  }
 }
-
 export default App;
